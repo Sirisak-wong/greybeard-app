1ef296d9880c702195143c8e804fa551b0d1d9a3
Sirisak-wong <79954390+Sirisak-wong@users.noreply.github.com>
2022-05-21 19:16:59 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import Home from './screens/Home';
-import Details from './screens/Details';
-import Register from './screens/Register';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button } from 'react-native-paper';
-import { View, Text } from 'react-native';
 import MyTabs from './components/MyTabs';
 
 const theme = {
@@ -16,26 +9,10 @@ const theme = {
   },
 };
 
-const Tab = createBottomTabNavigator();
-
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer theme={theme}>
-      
-      {/* <Register /> */}
-      {/* <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName='Register'
-      >
-        <Stack.Screen name='Register' component={Register} />
-        <Stack.Screen name='Home' component={Home} /> */}
-        {/* <Stack.Screen name='Home' component={MyTabs} /> */}
-        {/* <Stack.Screen name='Details' component={Details} /> */}
-      {/* </Stack.Navigator> */}
       <MyTabs />
     </NavigationContainer>
   );
